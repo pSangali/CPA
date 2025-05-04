2c39ab7c619addb00120355cc2f117a31ff797be
Jonatan <122944585+J0NATAN-Z@users.noreply.github.com>
2025-05-04 13:09:37 -0300

    finalizacao do filtro e preparacao para o front na main

--- Filtro.py
+++ Filtro.py
@@ -27,9 +27,13 @@ class Time:
 
     #me da o total de gols do time escolhido detodo o periodo
     def total_team_goals(self, team_name):
-        gols_casa = np.sum(self.brasil[brasil['home_team'] == team_name]['home_goal'])
-        gols_fora = np.sum(self.brasil[brasil['away_team'] == team_name]['away_goal'])
-        return f"Quantidade total de gols do {team_name} foi de {int(gols_casa + gols_fora)}"
+        gols_casa = int(np.sum(self.brasil[self.brasil['home_team'] == team_name]['home_goal']))
+        gols_fora = int(np.sum(self.brasil[self.brasil['away_team'] == team_name]['away_goal']))
+        total = gols_casa + gols_fora
+        return {
+            "time": team_name,
+            "total_gols": total
+        }
 
     # retorna as datas dos jogos entre 2 times
     def team(self, team_name1, team_name2):
@@ -37,44 +41,92 @@ class Time:
             ((self.brasil['home_team'] == team_name1) & (self.brasil['away_team'] == team_name2)) |
             ((self.brasil['home_team'] == team_name2) & (self.brasil['away_team'] == team_name1))
             ]
-        jogos_formatados = [
-            f"{i['datetime']}: {i['home_team']} {int(i['home_goal'])} x {int(i['away_goal'])} {i['away_team']}"
+        lista_jogos = [
+            {
+                "data": i["datetime"],
+                "casa": i["home_team"],
+                "gols_casa": int(i["home_goal"]),
+                "fora": i["away_team"],
+                "gols_fora": int(i["away_goal"])
+            }
             for _, i in jogos.iterrows()
         ]
-        return f"As partidas entre {team_name1} e {team_name2} foram:\n" + "\n".join(jogos_formatados)
+        return {
+            "times": [team_name1, team_name2],
+            "jogos": lista_jogos
+        }
 
     # lista todos os times de um estado específico
     def state(self, estado):
         estado = estado.upper()
         times_casa = self.brasil[self.brasil['home_team_state'] == estado]['home_team']
         times_fora = self.brasil[self.brasil['away_team_state'] == estado]['away_team']
-
-        # concatena os times e remove duplicatas com unique()
-        times = pd.concat([times_casa, times_fora]).unique()
-
-        return f"Os times que têm como origem {estado} são {len(times)}: \n" + "\n".join(sorted(times))
+        times = sorted(set(pd.concat([times_casa, times_fora]).unique()))
+        return {
+            "estado": estado,
+            "quantidade": len(times),
+            "times": times
+        }
 
     # retorna o número específico de gols de todos os times
     def mostraValor(self, value):
-        value = int(value)  # garante que seja número
+        value = int(value)
         jogos = self.brasil[(self.brasil['home_goal'] == value) | (self.brasil['away_goal'] == value)]
-        jogos_formatados = [
-            f"{i['datetime']}: {i['home_team']} {int(i['home_goal'])} x {int(i['away_goal'])} {i['away_team']}"
+        lista_jogos = [
+            {
+                "data": i["datetime"],
+                "casa": i["home_team"],
+                "gols_casa": int(i["home_goal"]),
+                "fora": i["away_team"],
+                "gols_fora": int(i["away_goal"])
+            }
             for _, i in jogos.iterrows()
         ]
-        return f"Os jogos com exatamente {value} gols foram:\n" + "\n".join(jogos_formatados)
+        return {
+            "gols_exatos": value,
+            "jogos": lista_jogos
+        }
 
     # mostra todos os jogos do time específico
     def mostraNome(self, team_name):
         jogos = self.brasil[(self.brasil['home_team'] == team_name) | (self.brasil['away_team'] == team_name)]
-        nome_formatados = [
-            f'data {i["datetime"]}: {i["home_team"]} {int(i["home_goal"])} x {i["away_team"]} {int(i["away_goal"])}'
-            for _, i  in jogos.iterrows()  
+        lista_jogos = [
+            {
+                "Adata": i["datetime"],
+                "casa": i["home_team"],
+                "gols_casa": int(i["home_goal"]),
+                "fora": i["away_team"],
+                "gols_fora": int(i["away_goal"])
+            }
+            for _, i in jogos.iterrows()
         ]
-        return f"Os jogos do {team_name} foram \n"  + "\n".join(nome_formatados)
+        return {"jogos": lista_jogos}
+
+    #retorna as n linhas que o usario queira do dataset
+    def mostraLinhas(self, n):
+        n = int(n)
+        linhas = self.brasil.head(n)
+        lista_linhas = [
+            {
+                "adata": i["datetime"],
+                "casa": i["home_team"],
+                "gols_casa": int(i["home_goal"]),
+                "estado_casa": i["home_team_state"],
+                "fora": i["away_team"],
+                "gols_fora": int(i["away_goal"]),
+                "estado_fora": i["away_team_state"]
+            }
+            for _, i in linhas.iterrows()
+        ]
+        return {
+            "quantidade": n,
+            "jogos": lista_linhas
+        }
+
 
 #print(Time(brasil).total_team_goals("Gremio"))
 #print(Time(brasil).team("Internacional", "Gremio"))
 #print(Time(brasil).state("sp"))
 #print(Time(brasil).mostraNome("Gremio"))
 #print(Time(brasil).mostraValor(6))
+#print(Time(brasil).mostraLinhas(10))
--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-from flask import Flask, request, jsonify
+import json, Filtro
+from flask import Flask, request, Response
+from Filtro import *
+
 app = Flask(__name__)
 
-#montar api
-#puxar os metodos/classes
+#usa um jsonify sem ordenacao
+def jsonify_ordered(data):
+    return Response(
+        response=json.dumps(data, ensure_ascii=False, indent=2, sort_keys=False),
+        mimetype='application/json'
+    )
+
+
+#montar api puxar os metodos/classes
+@app.route('/Time')
+def obter_times():
+    return jsonify_ordered(Filtro.Time(brasil).mostraLinhas(10))
+
+
+@app.route('/linhas/<int:n>')
+#exemplo linhas/5
+def primeiras_linhas(n):
+    return jsonify_ordered(Filtro.Time(brasil).mostraLinhas(n))
+
+@app.route('/gols/<time>')
+def gols_time(time):
+    return jsonify_ordered(Filtro.Time(brasil).total_team_goals(time))
+
+@app.route('/confronto')
+def confronto():
+    time1 = request.args.get("time1")
+    time2 = request.args.get("time2")
+    return jsonify_ordered(Filtro.Time(brasil).team(time1, time2))
+
+@app.route('/estado/<uf>')
+def times_do_estado(uf):
+    return jsonify_ordered(Filtro.Time(brasil).state(uf))
+
+@app.route('/gols/<int:n>/valor')
+def jogos_com_valor(n):
+    return jsonify_ordered(Filtro.Time(brasil).mostraValor(n))
+
+
 #front
+
+
+app.run(port=5000, host='localhost', debug=True)
